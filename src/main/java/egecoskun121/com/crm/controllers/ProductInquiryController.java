04f75da20b0322eba2b354722bad5d5327b17c5a
egecoskun121 <77891949+egecoskun121@users.noreply.github.com>
2022-10-24 17:26:56 +0300

    added ProductInquiryController

--- src/main/java/egecoskun121/com/crm/controllers/ProductInquiryController.java
+++ src/main/java/egecoskun121/com/crm/controllers/ProductInquiryController.java
@@ -1,11 +1,13 @@
 package egecoskun121.com.crm.controllers;
 
+import egecoskun121.com.crm.model.DTO.ProductInquiryDTO;
+import egecoskun121.com.crm.model.entity.ProductInquiry;
 import egecoskun121.com.crm.model.mapper.ProductInquiryMapperImpl;
 import egecoskun121.com.crm.services.ProductInquiryService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/api/v1/inquiry")
@@ -19,12 +21,39 @@ public class ProductInquiryController {
         this.productInquiryMapper = productInquiryMapper;
     }
 
-    @GetMapping("/showList")
+    @RequestMapping(path = "/showList")
     public ModelAndView showProductInquiryList(){
         ModelAndView mav = new ModelAndView("productInquiryList");
         mav.addObject("productInquiry",productInquiryService.getAllProductInquiries());
         return mav;
     }
+
+    @RequestMapping(path="/updateInquiry")
+    public RedirectView updateInquiry(@RequestParam Long id,@ModelAttribute ProductInquiryDTO productInquiryDTO){
+
+        productInquiryService.updateProductInquiryById(id, productInquiryDTO);
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("http://localhost:8093/api/v1/inquiry/showList");
+
+        return redirectView;
+    }
+
+
+
+    @RequestMapping(path = "/deleteInquiry")
+    public RedirectView deleteProductInquiry(@RequestParam Long id){
+
+        productInquiryService.deleteProductInquiryById(id);
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("http://localhost:8093/api/v1/inquiry/showList");
+
+        return redirectView;
+    }
+
+
+
+
+
 
 
 }
