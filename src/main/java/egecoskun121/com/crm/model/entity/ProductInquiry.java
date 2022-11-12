89ae2f6831185915514c7ae4129f13dceb30fa7d
egecoskun121 <77891949+egecoskun121@users.noreply.github.com>
2022-11-12 16:23:48 +0300

    Added user mapping

--- src/main/java/egecoskun121/com/crm/model/entity/ProductInquiry.java
+++ src/main/java/egecoskun121/com/crm/model/entity/ProductInquiry.java
@@ -12,19 +12,19 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
 import java.sql.Timestamp;
-import java.util.UUID;
+
 
 @SqlResultSetMapping(name="ProductInquiry.productInquiryResult", classes = {
         @ConstructorResult(targetClass = ProductInquiry.class,
                 columns = {@ColumnResult(name="ID",type = Long.class),@ColumnResult(name="ANSWER"),
                         @ColumnResult(name="CREATED_DATE",type = String.class),@ColumnResult(name = "DETAILS") ,
                         @ColumnResult(name = "LAST_MODIFIED_DATE",type = String.class),@ColumnResult(name = "MAIL"),
-                        @ColumnResult(name = "PRODUCT_INQUIRY_ANSWER")})
+                        @ColumnResult(name = "PRODUCT_INQUIRY_ANSWER"),@ColumnResult(name="PRODUCT_NAME")})
 })
 @NamedNativeQuery(
         name = "ProductInquiry.productInquiryResult",
         resultClass = ProductInquiry.class,
-        query = "SELECT p.ID,p.ANSWER,p.CREATED_DATE,p.DETAILS,p.LAST_MODIFIED_DATE,p.MAIL, p.PRODUCT_INQUIRY_ANSWER FROM PRODUCT_INQUIRY AS p INNER JOIN  USERS_PRODUCT_INQUIRIES  AS u ON u.PRODUCT_INQUIRIES_ID = p.ID WHERE u.USER_ID= {SELECT ID FROM USERS WHERE USER_NAME= (:username)}" ,
+        query = "SELECT p.ID,p.ANSWER,p.CREATED_DATE,p.DETAILS,p.LAST_MODIFIED_DATE,p.MAIL, p.PRODUCT_INQUIRY_ANSWER,p.PRODUCT_NAME FROM PRODUCT_INQUIRY AS p INNER JOIN  USERS_PRODUCT_INQUIRIES  AS u ON u.PRODUCT_INQUIRIES_ID = p.ID WHERE u.USER_ID= {SELECT ID FROM USERS WHERE USER_NAME= (:username)}" ,
         resultSetMapping = "ProductInquiry.productInquiryResult")
 @Data
 @Entity
@@ -32,7 +32,7 @@ import java.util.UUID;
 @NoArgsConstructor
 public class ProductInquiry {
 
-    public ProductInquiry(Long id,String answer,String createdDate,String details,String lastModifiedDate,String mail,Integer productInquiryAnswer){
+    public ProductInquiry(Long id,String answer,String createdDate,String details,String lastModifiedDate,String mail,Integer productInquiryAnswer,String productName){
         this.id=id;
         this.answer=answer;
         this.createdDate=Timestamp.valueOf(createdDate);
@@ -46,6 +46,7 @@ public class ProductInquiry {
             }
             i++;
         }
+        this.productName=productName;
     }
 
     @Id
@@ -72,4 +73,8 @@ public class ProductInquiry {
     private String productName;
 
     private ProductInquiryAnswer productInquiryAnswer;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 }
