8148d75440ceaadbbc7b7b3dac9e1f1987f2c2b7
BurakKolay <burakkolay487@gmail.com>
2022-11-14 14:04:36 +0300

    added scheduled and async send mail

--- src/main/java/egecoskun121/com/crm/services/EmailSenderService.java
+++ src/main/java/egecoskun121/com/crm/services/EmailSenderService.java
@@ -1,16 +1,28 @@
 package egecoskun121.com.crm.services;
 
+import egecoskun121.com.crm.model.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Service
+@EnableScheduling
+@EnableAsync
 public class EmailSenderService {
 
     @Autowired
     private JavaMailSender mailSender;
+    @Autowired
+    private UserService userService;
 
     @Value("mail.sender")
     String senderEmail;
@@ -27,7 +39,21 @@ public class EmailSenderService {
         message.setText(body);
 
         mailSender.send(message);
+    }
 
-
+    @Scheduled(cron = "* * * */7 * *")
+    @Async
+    public void sendMail(){
+        for (User user: userService.getAllUsers()) {
+            if(!user.getIsUserHappy()){
+                sendEmail(user.getEmail(),"Are you happy with our products?","Hey there," +
+                        "We just wanted to say hi and ask if you need any help about our products." +
+                        "Just let us know if you need any help ! :)");
+                DateTimeFormatter formatter=DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+                String formattedDate = LocalDateTime.now().format(formatter);
+                System.out.println("Mail gonderildi"+ formattedDate);
+            }
+            user.setIsPopupShowed(false);
+        }
     }
 }
