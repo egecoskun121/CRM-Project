fd82a9eb056e700f384bbbf57cabb904ea62d998
egecoskun121 <77891949+egecoskun121@users.noreply.github.com>
2022-11-26 17:39:29 +0300

    added ProductRecommendationController

---
+++ src/main/java/egecoskun121/com/crm/controllers/ProductRecommendationController.java
@@ -0,0 +1,31 @@
+package egecoskun121.com.crm.controllers;
+
+import egecoskun121.com.crm.model.entity.Product;
+import egecoskun121.com.crm.services.ProductService;
+import egecoskun121.com.crm.services.UserService;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/api/v1/recommendation")
+public class ProductRecommendationController {
+    private ProductService productService;
+    private UserService userService;
+
+    public ProductRecommendationController(ProductService productService, UserService userService) {
+        this.productService = productService;
+        this.userService = userService;
+    }
+
+
+    @RequestMapping(value = "")
+    public ModelAndView getRecommendations(@RequestParam String username){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("product",productService.getMaxCategoryListOfProduct(username));
+        return modelAndView;
+    }
+
+}
